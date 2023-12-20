5db16412bcfeadf220b12486000976a31051792e
Aditya Kamble <adityakamble6101@gmail.com>
2023-12-20 21:04:50 +0530

    working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,66 @@
-// DOM Elements
+const API_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
 
-const input = document.getElementById('input');
-const submitBtn = document.querySelector('.submit-btn');
+// DOM Elements
+let searchEl = document.getElementById("input");
+let submitButtonEl = document.querySelector(".submit-btn");
+let dataViewEl = document.querySelector(".user-data-card");
 
 // Dom Elements for injecting fetched details
+let showIpAdd = document.getElementById("ip-address");
+let showLoc = document.getElementById("location");
+let showTime = document.getElementById("timezone");
+let showISP = document.getElementById("isp");
 
-const showIpAdd = document.getElementById('ip-address');
-const showLoc = document.getElementById('location');
-const showTime = document.getElementById('timezone');
-const showISP = document.getElementById('isp');
+let map = L.map("map");
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 
-alert('⚠ This site i currently under development!!⚠ Please visit later.');
+function renderMap({ lat, lng }) {
+  map.setView([lat, lng], 10);
+  L.marker([lat, lng]).addTo(map);
+}
+
+async function getIpInfo(value = "") {
+  let URL = value ? `${API_URL}&domain=${value}` : API_URL;
+
+  let res = await fetch(URL);
+  let data = await res.json();
+  let { lat, lng, city, country, timezone } = data.location;
+
+  updateDataValues({
+    ip: data.ip,
+    isp: data.isp,
+    city,
+    country,
+    timezone,
+  });
+
+  renderMap({ lat, lng });
+}
+
+function updateDataValues(data) {
+  let { ip, isp, city, country, timezone } = data;
+
+  showIpAdd.innerText = ip;
+  showISP.innerText = isp;
+  showLoc.innerText = `${city} - ${country}`;
+  showTime.innerText = timezone;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  getIpInfo();
+});
+
+submitButtonEl.addEventListener("click", () => {
+  let value = searchEl.value;
+
+  if (!value) {
+    return alert("Please enter valid term");
+  }
+  // TODO add validation here
+
+  getIpInfo(value);
+});
